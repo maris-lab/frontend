7f5fefabeef9538c63844960b153ddf72976788e
maris-lab <mariasilva18042005@gmail.com>
2024-08-13 10:23:51 -0300

    public class HelloWorld{ // Aqui é criada a classe HelloWorld     public static void main(String[] args) { // Aqui é criada a classe main         System.out.println("Hello World!!!"); // Aqui é " impressa"uma linha com texto "HelloWorld"     } // Aqui encerra o bloco de código do método main } // Aqui encerra o bloco de código de classe "HelloWorld" /**   * public: declaração que "exporta" e permite que a classe/método/variável seja importada/reutilizada em outras classes/objetos  * classe: declaração de classse(que poderá se tornar um objeto)  * HelloWorld:nome da classe, seguindo o padrão de nomeclatura PascalCase  * { ( "abre chave"): serve para declarar o inicio de um bloco de códigos/ações  * } ("fecha chave"): serve para declarar o fim de um bloco de códigos/ações  */  public class HelloWorld { // Aqui é criada a classe HellloWorld   /**    * static: declaração de método somente leitura, o qual não poderá ser editado, exceto pelo uso de @Overrride   * void: modo protegido de um método, qual não retornará qualquer valor    *main: nome do método "executor" *(abre parenteses):serve para declarar o início das declarações de parâmetros de um método/função  *) (fecha parenteses): serve para declarar o fim das declarações de parametros de um método/função * @param args * System: invoca a classe de sistema  * . ( ponto): serve como operador de invocação de método  * out: invoca a subclasse de saída do sistema  * println: invoca o método, ou função abstrata, o qual irá "imprimir"  na tela um string   * "HelloWorld": texto (string) que será "impresso" na tela   * ; ( ponto e vírgula): serve para encerrar uma linha de código) * /

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld{ // Aqui é criada a classe HelloWorld
+    public static void main(String[] args) { // Aqui é criada a classe main
+        System.out.println("Hello World!!!"); // Aqui é " impressa"uma linha com texto "HelloWorld"
+    } // Aqui encerra o bloco de código do método main
+} // Aqui encerra o bloco de código de classe "HelloWorld"
+/** 
+ * public: declaração que "exporta" e permite que a classe/método/variável seja importada/reutilizada em outras classes/objetos
+ * classe: declaração de classse(que poderá se tornar um objeto)
+ * HelloWorld:nome da classe, seguindo o padrão de nomeclatura PascalCase
+ * { ( "abre chave"): serve para declarar o inicio de um bloco de códigos/ações
+ * } ("fecha chave"): serve para declarar o fim de um bloco de códigos/ações
+ */
+ public class HelloWorld { // Aqui é criada a classe HellloWorld 
+ /** 
+  * static: declaração de método somente leitura, o qual não poderá ser editado, exceto pelo uso de @Overrride
+  * void: modo protegido de um método, qual não retornará qualquer valor 
+  *main: nome do método "executor"
+*(abre parenteses):serve para declarar o início das declarações de parâmetros de um método/função 
+*) (fecha parenteses): serve para declarar o fim das declarações de parametros de um método/função
+* @param args
+* System: invoca a classe de sistema 
+* . ( ponto): serve como operador de invocação de método 
+* out: invoca a subclasse de saída do sistema 
+* println: invoca o método, ou função abstrata, o qual irá "imprimir"  na tela um string 
+ * "HelloWorld": texto (string) que será "impresso" na tela 
+ * ; ( ponto e vírgula): serve para encerrar uma linha de código)
+* /
